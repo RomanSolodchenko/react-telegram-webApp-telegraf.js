9bdd0bed29c4d397aa2dd859f06f377f64f4f499
Roma2021 <IT2021rweb@gmail.com>
2022-05-18 23:24:51 +0300

    real button, card,cart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
+import React,{useState} from 'react';
 import './App.css';
-import Button from './components/Button/Button';
+import Cart from './components/Cart/Cart';
 import Card from './components/Card/Card';
+
 const { getData } = require ("./db/db");
+const foods = getData(); // к переменной присваен всю бд с товаром
 
 
 function App() {
+
+  const [cartItems, setCartItems] = useState([]); // состяние карточки старт 0 потом сумма
+
+
+ // функция для добавление в карточку(типа корзина товаров)
+ const onAdd = (food) => {
+  const exist = cartItems.find((x) => x.id === food.id); // если уже есть в карточке 
+  if (exist) {
+    setCartItems(
+      cartItems.map((x) =>
+        x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x  // если элемент есть то прибав 1 
+      )
+    );
+  } else {
+    setCartItems([...cartItems, { ...food, quantity: 1 }]);
+  }
+};
+//ф-я удаление из карточки товара
+const onRemove = (food) => {
+  const exist = cartItems.find((x) => x.id === food.id);
+  if (exist.quantity === 1) {
+    setCartItems(cartItems.filter((x) => x.id !== food.id));
+  } else {
+    setCartItems(
+      cartItems.map((x) =>
+        x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+      )
+    );
+  }
+};
+
   return (<>
-  
-  
+  <h1 className='heading'>Order fooood</h1>
+<Cart cartItems={cartItems} />   {/* типа корзина в которой цена на выбранные товары, cartitems хук состояния, и две функции добав\удалить */}
+<div className="cards__container">
+{foods.map((food) => {
+    return <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove}  />
+  })}
+</div>
+ 
 
 
-  <Button title={"Add"} disable={false} type={"add"} />
-  <Button title={"Remove"} disable={false} type={"remove"} />
-  <Button title={"Checkout"} disable={false} type={"checkout"} />
-  
-  <Card />
   </>
-    
-      
   );
 }
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import './Card.css';
+import React, { useState } from "react";
+import "./Card.css";
+import Button from "../Button/Button";
 
+function Card({ food, onAdd, onRemove }) {
+  const [count, setCount] = useState(0); // состояние счетчика
 
-function Card() {
+  const { title, Image, price, id } = food;
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+    onAdd(food);
+  };
+  // увел на 1(умен на 1) при бтн онклик колво товара
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+    onRemove(food);
+  };
+
   return (
-    <div>Card</div>
-  )
+    <div className="card">
+      <span
+        className={`${count !== 0 ? "card__badge" : "card__badge--hidden"}`}
+      >
+        {count}
+      </span>
+      {/* // если счетчик не равен 0 то появляется иконка с кол-во выбраных элементов вверху карточки */}
+      <div className="image__container">
+        <img src={Image} alt={title} />
+      </div>
+      <h4 className="card__title">
+        {title} . <span className="card__price">$ {price}</span>
+      </h4>
+      {/* вставка после изображ название и цены товара  */}
+      <div className="btn-container">
+        {/* добавление кнопки +\-  */}
+        <Button title={"+"} type={"add"} onClick={handleIncrement} />
+        {count !== 0 ? (
+          <Button title={"-"} type={"remove"} onClick={handleDecrement} />
+        ) : ( " " )}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
